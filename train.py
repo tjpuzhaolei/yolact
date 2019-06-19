d27ab4d5150d7ca5d12a950f0075b0886d9b9171
zl <zl9@meitu.com>
2019-06-19 20:23:40 +0800

    no message

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ def str2bool(v):
 
 parser = argparse.ArgumentParser(
     description='Yolact Training Script')
-parser.add_argument('--batch_size', default=5, type=int,
+parser.add_argument('--batch_size', default=4, type=int,
                     help='Batch size for training')
 parser.add_argument('--resume', default=None, type=str,
                     help='Checkpoint state_dict file to resume training from. If this is "interrupt"'\
